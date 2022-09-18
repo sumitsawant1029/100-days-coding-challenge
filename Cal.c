2a807a8c2901052dc98cf74c69c8627bda854a1f
sumitsawant1029 <sumitsawant1029@gmail.com>
2022-09-18 13:47:12 +0530

    Added Basic Code

--- Cal.c
+++ Cal.c
@@ -1,2 +1,28 @@
 // Program To Calculate Easy Operations using C
 
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("****************Operation******************");
+    printf("Enter two Operands: ");
+    scanf("%d %d",&a,&b);
+    printf("/n");
+    printf("Enter The Operant (*,/,+,-,%):");
+    scanf("%c",&c);
+
+    switch ()
+    {
+    case '*': mul(a,b);
+              break;
+    case '/': div(a,b);
+              break;
+    case '-': sub(a,b);
+              break;
+    case '+': add(a,b);
+              break;
+    default:  printf("You Entered A Wrong Choice");
+        break;
+    }
+
+}
