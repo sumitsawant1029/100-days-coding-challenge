c61cea170a65e78caaee8b19f7f710157a89db29
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-23 23:32:19 -0700

    Added Doubly Link List

---
+++ Doubly.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int e=0,ch;
+    while(e!=1){
+        printf("******************Doubly LinkList*******************");
+        printf("1) Insert From Beginning\n");
+        printf("2) Insert From End\n");
+        printf("3) Insert At Position\n");
+        printf("4) Delete From Beginning\n");
+        printf("5) Delete From End\n");
+        printf("6) Delete At Position\n");
+        printf("7) Exit\nEnter Your Choice:");
+        scanf("%d",&ch);
+
+        switch(ch){
+            case 1: 
+
+
+
+
+
+        }
+        
+
+
+
+
+
+    }
+
+
+
+
+
+}
